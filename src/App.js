3382ea01c582a72d6f2c5f806e43f29984df027b
manriqueluis98 <manriqueleo98@gmail.com>
2023-06-28 15:28:27 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import Home from "./components/Home";
+import Stocks from "./components/Stocks";
+import Movies from "./components/Movies";
+
+import { Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="main">
+      <h1>App Component</h1>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="/stocks">
+          <Stocks />
+        </Route>
+
+        <Route path="/movies">
+          <Movies />
+        </Route>
+
+        <Route>
+          <h1>Page not found</h1>
+        </Route>
+      </Switch>
+      {/* <Home />
+      <Stocks />
+      <Movies /> */}
+    </div>
+  );
+}
+
+export default App;
